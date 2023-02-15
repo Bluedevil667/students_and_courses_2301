46841be2643f98455a18e28bb444e5a88cfa59b9
Logan Wilson <loganwilson777@gmail.com>
2023-02-15 09:37:31 -0700

    Student class wth log_score method

--- lib/student.rb
+++ lib/student.rb
@@ -1,1 +1,19 @@
+class Student
+  attr_reader :name,
+              :age,
+              :scores
 
+  def initialize(student_attributes)
+    @name = student_attributes[:name]
+    @age = student_attributes[:age]
+    @scores = []
+  end
+
+  def log_score(score)
+    @scores << score
+  end
+
+  def grade
+    
+  end
+end
