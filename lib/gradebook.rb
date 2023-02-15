5924d4743e511404f994c3f6867badc19d2f951f
Logan Wilson <loganwilson777@gmail.com>
2023-02-15 10:54:57 -0700

    Iteration 3

--- lib/gradebook.rb
+++ lib/gradebook.rb
@@ -1,1 +1,26 @@
+class Gradebook
+  attr_reader :instructor,
+              :courses
 
+  def initialize(instructor)
+    @instructor = instructor
+    @courses = []
+    @students_below = []
+  end
+
+  def add_course(course)
+    @courses << course
+  end
+
+  def students_below
+    @courses.each do |course|
+      students = course.students
+      students.each do |student|
+        if student.grade <= 60
+          @students_below << student
+        end
+      end
+    end
+    @students_below
+  end
+end
